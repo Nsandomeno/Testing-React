031c284f7f928a4b9de7d07da6593f43ebf89bc2
Nicholas Sandomeno <nicholassandomeno@Nicholass-Air.home>
2020-01-27 17:20:31 -0500

    Test written, but not found.

--- star-wars-app/src/setupTests.js
+++ star-wars-app/src/setupTests.js
@@ -2,4 +2,33 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect';
+import React from 'react';
+import { render, fireEvent, wait } from '@testing-library/jest-dom/extend-expect';
+// From API Folder
+import { getData as mockGetData } from './api/index.js';
+// Component itself 
+import StarWarsCharacters from './components/StarWarsCharacters.js';
+
+jest.mock('./api/index')
+
+test('Goes to next; Goes to previous', async () => {
+    mockGetData.mockResolveValueOnce({ results: [
+        {
+        name: 'Luke Skywalker',
+        height: '172',
+        mass: '77',
+        hair_color: 'blond',
+        skin_color: 'fair'
+    }
+]})
+
+const { getByText } = render(<StarWarsCharacters />)
+const nextButton = getByText(/next/i)
+const prevButton = getByText(/previous/i)
+
+fireEvent.click(nextButton)
+fireEvent.click(prevButton)
+expect(mockGetData).toHaveBeenCalledTimes(1)
+
+wait(() => expect(getByText(/darth/i)))
+})
