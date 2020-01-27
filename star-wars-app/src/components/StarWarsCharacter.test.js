66d48aaff842c261481a2b180f3be1f86603ca23
Nicholas Sandomeno <nicholassandomeno@Nicholass-Air.home>
2020-01-27 17:48:37 -0500

    Test running and passing

--- star-wars-app/src/components/StarWarsCharacter.test.js
+++ star-wars-app/src/components/StarWarsCharacter.test.js
@@ -3,26 +3,29 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import React from 'react';
-import { render, fireEvent, wait } from '@testing-library/jest-dom/extend-expect';
-// From API Folder
-import { getData as mockGetData } from './api/index.js';
-// Component itself 
-import StarWarsCharacters from './components/StarWarsCharacters.js';
+import { render, fireEvent, wait } from '@testing-library/react';
 
-jest.mock('./api/index')
+// From API Folder
+import { getData as mockGetData } from '../api/index.js';
+// Component itself 
+import StarWarsCharacters from './StarWarsCharacters.js';
+
+
+jest.mock('../api/index')
 
 test('Goes to next; Goes to previous', async () => {
-    mockGetData.mockResolveValueOnce({ results: [
+    mockGetData.mockResolvedValueOnce(
+        { results: [
         {
-        name: 'Luke Skywalker',
-        height: '172',
-        mass: '77',
-        hair_color: 'blond',
-        skin_color: 'fair'
+        name: ''
+        }],
+        next: 'abcde',
+        previous: 'now'
     }
-]})
+)
 
 const { getByText } = render(<StarWarsCharacters />)
+
 const nextButton = getByText(/next/i)
 const prevButton = getByText(/previous/i)
 
@@ -30,5 +33,5 @@ fireEvent.click(nextButton)
 fireEvent.click(prevButton)
 expect(mockGetData).toHaveBeenCalledTimes(1)
 
-wait(() => expect(getByText(/darth/i)))
+wait(() => expect(getByText(/'Darth'/i)))
 })
