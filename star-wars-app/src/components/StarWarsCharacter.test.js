9d7d34c984f456ac1f92177d8605586c5fabc203
Nicholas Sandomeno <nicholassandomeno@Nicholass-Air.home>
2020-01-28 00:10:26 -0500

    testing render, state change, and async

--- star-wars-app/src/components/StarWarsCharacter.test.js
+++ star-wars-app/src/components/StarWarsCharacter.test.js
@@ -3,35 +3,82 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import React from 'react';
-import { render, fireEvent, wait } from '@testing-library/react';
+import { render, fireEvent, wait, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 
 // From API Folder
 import { getData as mockGetData } from '../api/index.js';
 // Component itself 
 import StarWarsCharacters from './StarWarsCharacters.js';
 
-
+afterEach(cleanup)
 jest.mock('../api/index')
+//  mockGetData.mockResolvedValueOnce(
+//     { results: [
+//     {
+//     name: 'test name',
+//     url: 'test url'
+//     }],
+//     next: 'test next',
+//     previous: null
+// }
+// )
 
 test('Goes to next; Goes to previous', async () => {
+    // async before () on line 27
+
     mockGetData.mockResolvedValueOnce(
         { results: [
         {
-        name: ''
+        name: 'test name',
+        url: 'test url'
         }],
-        next: 'abcde',
-        previous: 'now'
+        next: 'test next',
+        previous: null
+
     }
 )
 
 const { getByText } = render(<StarWarsCharacters />)
 
-const nextButton = getByText(/next/i)
-const prevButton = getByText(/previous/i)
+// const nextButton = getByText(/next/i)
+// const prevButton = getByText(/previous/i)
 
-fireEvent.click(nextButton)
-fireEvent.click(prevButton)
+// fireEvent.click(nextButton)
+// fireEvent.click(prevButton)
 expect(mockGetData).toHaveBeenCalledTimes(1)
 
-wait(() => expect(getByText(/'Darth'/i)))
+await wait(() =>  expect(getByText(/test name/i)))
+// await before wait on line 48
 })
+
+test('changing state with clicks', async () => {
+    const { getByText } = render(<StarWarsCharacters />)
+    mockGetData.mockResolvedValueOnce(
+        { results: [
+        {
+        name: 'test name',
+        url: 'test url'
+        }],
+        next: 'test next',
+        previous: null
+    }
+)
+     
+    const nextButton = getByText(/next/i)
+    // let prevButton = getByText(/previous/i)
+
+    expect(mockGetData).toHaveBeenCalledTimes(1)
+    fireEvent.click(nextButton)
+
+    await wait(expect(getByText(/test url/i)))
+})
+
+test('render check on Previous Button', async () => {
+    const { getByText, queryByTestId } = render(<StarWarsCharacters />)
+
+    const prevButton = queryByTestId(/prevbtn/i)
+    // expect(data).toHaveBeenCalledTimes(1)
+    await wait(expect(prevButton).toBeVisible())
+})
+
